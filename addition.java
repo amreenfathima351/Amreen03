a07eb5bd94720af12f9b94ab73f18a4107e5006d
amreenfathima351 <112692713+amreenfathima351@users.noreply.github.com>
2022-09-19 20:00:22 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,17 @@
+package firstprogram;
+
+public class addition {
+
+	public static void main(String[] args) {
+		System.out.println("enter two numbers");
+		int first = 100;
+		int second = 200;
+		System.out.println(first + "" + second);
+		//addition two numbers
+		int sum = first + second ;
+		System.out.println("the sum:"+sum);
+		
+
+	}
+
+}

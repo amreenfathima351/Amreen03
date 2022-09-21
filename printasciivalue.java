689e73dfd582a50d1a2c06add1aeedcdb7386449
amreenfathima351 <112692713+amreenfathima351@users.noreply.github.com>
2022-09-21 21:20:37 +0530

    Add files via upload

---
+++ printasciivalue.java
@@ -0,0 +1,14 @@
+package printasciivalue;
+
+public class printasciivalue {
+
+	public static void main(String[] args) {
+		char ch = 'H';
+		int ascii = ch;
+		int castascii = (int) ch;
+		System.out.println("The ASCII value of " + ch + " is :"+ascii);
+		System.out.println("The ASCII value of " + ch + " is :"+castascii);
+
+	}
+
+}

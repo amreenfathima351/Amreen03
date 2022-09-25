60eed92eb9c7bb4dc66aeb10a8d7fe9ec467040a
amreenfathima351 <112692713+amreenfathima351@users.noreply.github.com>
2022-09-25 13:54:35 +0530

    Add files via upload

---
+++ c io1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int testinteger = 5;
+    printf("number = %d",testinteger);
+    return 0;
+}
---
+++ c io3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   char ch = 'a';
+   printf("character = %c",ch);
+    return 0;
+}

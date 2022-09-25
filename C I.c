8022b17f930001d32054e95eb353e5c2e899f770
amreenfathima351 <112692713+amreenfathima351@users.noreply.github.com>
2022-09-25 13:47:19 +0530

    Add files via upload

---
+++ C I.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int testinteger;
+    printf("Enter an integer:");
+    scanf("%d",&testinteger);
+    printf("Number = %d",testinteger);
+    return 0;
+}
